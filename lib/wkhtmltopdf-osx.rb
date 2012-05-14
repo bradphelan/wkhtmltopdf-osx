c1da0739ae48ed6c121f400972dfd6a3d9376cf7
Jason Kotchoff <cornflakesuperstar@hotmail.com>
2012-05-14 16:55:30 +1000

    The WKHTMLTOPDF_PATH variable is caching to the darwin file on heroku

--- lib/wkhtmltopdf-osx.rb
+++ lib/wkhtmltopdf-osx.rb
@@ -2,18 +2,18 @@
 on_heroku = !ENV.keys.select{|k| k.starts_with?("HEROKU")}.empty?
 executable = on_heroku ? "wkhtmltopdf-linux-amd64" : "wkhtmltopdf-darwin"
 
-WKHTMLTOPDF_PATH = File.expand_path "../../bin/#{executable}", __FILE__
+wkhtmltopdf_path = File.expand_path "../../bin/#{executable}", __FILE__
 
 begin
   require 'pdfkit'
   PDFKit.configure do |config|
-    config.wkhtmltopdf = WKHTMLTOPDF_PATH
+    config.wkhtmltopdf = wkhtmltopdf_path
   end
 rescue LoadError
 end
 
 begin
   require 'wicked_pdf'
-  WickedPdf.config[:exe_path] = WKHTMLTOPDF_PATH
+  WickedPdf.config[:exe_path] = wkhtmltopdf_path
 rescue LoadError
 end
